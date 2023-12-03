de4c440c841c80a1a527ed7178abfafeb7b52560
timmy <timothy.hjort@outlook.com>
2023-12-03 22:29:55 +0100

    Add test pdf for date mode, fixed bug where we only used first year

--- src/core/production/dates.rs
+++ src/core/production/dates.rs
@@ -4,6 +4,7 @@ pub struct DateProducer {
     current: usize,
     end: usize,
     inner: Box<dyn Iterator<Item = String>>,
+    counter: usize,
 }
 
 /// Pregenerates all naively "valid" combinations of days and months.
@@ -35,31 +36,40 @@ impl DateProducer {
     pub fn new(start: usize, end: usize) -> Self {
         // Note that we will always have the same amount of months and days since we are doing this naively.
         // The only part that varies is the year, so why not pre-generate days and months?
-        let dates = pregenerate_dates().into_iter();
+        let dates = pregenerate_dates().into_iter().cycle();
 
         Self {
             current: start,
             end,
             inner: Box::from(dates),
+            counter: 0,
         }
     }
 }
 
 impl Producer for DateProducer {
     fn next(&mut self) -> Result<Option<Vec<u8>>, String> {
-        while self.current <= self.end {
-            let next = self.inner.next();
-            match next {
-                Some(datemonth) => {
-                    let password = format!("{}{}", datemonth, self.current).into_bytes();
-                    return Ok(Some(password));
-                }
-                None => {
-                    self.current += 1;
-                }
-            };
+        if self.current > self.end {
+            debug!("stopping at year {}", self.current);
+            Ok(None)
+        } else {
+            // We need to know when we have finished the year so we can start working on the next
+            if self.counter == 12 * 31 {
+                self.counter = 0;
+                self.current += 1;
+            } else {
+                self.counter += 1;
+            }
+
+            let next = self.inner.next().unwrap();
+
+            let password = format!("{:04}{:04}", next, self.current).into_bytes();
+            debug!(
+                "Sending {} from DateProducer",
+                String::from_utf8_lossy(&password)
+            );
+            Ok(Some(password))
         }
-        return Ok(None);
     }
 
     fn size(&self) -> usize {
