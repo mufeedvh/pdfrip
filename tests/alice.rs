55a6884bff1ec4846af19a0615c998497a83077c
timmy <timothy.hjort@outlook.com>
2024-02-10 13:28:05 +0100

    Code cleanup

--- tests/alice.rs
+++ tests/alice.rs
@@ -1,8 +1,8 @@
-use cli_interface::{self, arguments, Code};
+use cli_interface::{arguments, entrypoint, Code};
 
 #[test]
 fn successful_crack() {
-    let args = cli_interface::arguments::Arguments {
+    let args = arguments::Arguments {
         number_of_threads: 4,
         filename: "examples/ALICE_BANK_STATEMENT.pdf".to_string(),
         subcommand: arguments::Method::CustomQuery(arguments::CustomQueryArgs {
@@ -11,14 +11,14 @@ fn successful_crack() {
         }),
     };
 
-    let res = cli_interface::entrypoint(args).expect("An error occured when cracking file");
+    let res = entrypoint(args).expect("An error occured when cracking file");
 
     assert!(matches!(res, Code::Success), "Failed cracking file.")
 }
 
 #[test]
 fn failed_crack() {
-    let args = cli_interface::arguments::Arguments {
+    let args = arguments::Arguments {
         number_of_threads: 4,
         filename: "examples/ALICE_BANK_STATEMENT.pdf".to_string(),
         subcommand: arguments::Method::CustomQuery(arguments::CustomQueryArgs {
@@ -27,6 +27,6 @@ fn failed_crack() {
         }),
     };
 
-    let res = cli_interface::entrypoint(args).expect("An error occured when cracking file");
+    let res = entrypoint(args).expect("An error occured when cracking file");
     assert!(matches!(res, Code::Failure), "We expected to fail this.")
 }
